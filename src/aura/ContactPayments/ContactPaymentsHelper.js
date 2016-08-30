bc79a6724de546e7fc44ac678da3448f215acf4e
Kalolo <kalolo@gmail.com>
2016-08-30 15:33:45 -0700

    ident changes

--- src/aura/ContactPayments/ContactPaymentsHelper.js
+++ src/aura/ContactPayments/ContactPaymentsHelper.js
@@ -1,46 +1,46 @@
 ({
-  getPayments : function(component) {
+    getPayments: function(component) {
 
-		var getPaymentsAction = component.get("c.getPayments");
+        var getPaymentsAction = component.get("c.getPayments");
 
-		getPaymentsAction.setCallback(this, function(response){
-			component.set("v.paymentList", response.getReturnValue());
-		});
+        getPaymentsAction.setCallback(this, function(response) {
+            component.set("v.paymentList", response.getReturnValue());
+        });
 
-		$A.enqueueAction(getPaymentsAction);
+        $A.enqueueAction(getPaymentsAction);
 
-	},
+    },
 
 
-	addPayment  : function(component) {
-		var amount           = component.find("amount").get("v.value"),
-	      description      = component.find("description").get("v.value"),
-		    addPaymentAction = component.get("c.addPayment");
+    addPayment: function(component) {
+        var amount = component.find("amount").get("v.value"),
+            description = component.find("description").get("v.value"),
+            addPaymentAction = component.get("c.addPayment");
 
-		addPaymentAction.setParams({
-				"amount":   amount,
-				"description": component.find("description").get("v.value")
-		});
+        addPaymentAction.setParams({
+            "amount": amount,
+            "description": component.find("description").get("v.value")
+        });
 
-		addPaymentAction.setCallback(this, function(response) {
-				this.updateLayout(response, component);
-				this.getPayments(component);
-				this.showToast('Success!', 'Payment added successfully');
-		});
+        addPaymentAction.setCallback(this, function(response) {
+            this.updateLayout(response, component);
+            this.getPayments(component);
+            this.showToast('Success!', 'Payment added successfully');
+        });
 
-		$A.enqueueAction(addPaymentAction);
-	},
+        $A.enqueueAction(addPaymentAction);
+    },
 
-	updateLayout: function(response, component) {
-			var data = JSON.parse(response.getReturnValue());
-	},
+    updateLayout: function(response, component) {
+        var data = JSON.parse(response.getReturnValue());
+    },
 
-	showToast : function(title, message) {
-    var toastEvent = $A.get("e.force:showToast");
-    toastEvent.setParams({
-        "title": title,
-        "message": message
-    });
-    toastEvent.fire();
-}
+    showToast: function(title, message) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": title,
+            "message": message
+        });
+        toastEvent.fire();
+    }
 })
